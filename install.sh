4d19c423063358687272b2cb23e006e3fc2b1c14
Eric Moore <EricGittaMoore@duck.com>
2025-05-03 19:22:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo mkdir /usr/share/fonts/linux_fonts
 sudo cp ./*.ttf /usr/share/fonts/linux_fonts
 sudo cp ./*.ttc /usr/share/fonts/linux_fonts
 cd /usr/share/fonts/linux_fonts
-sudo mkfontscale
-sudo mkfontdir
+# sudo mkfontscale
+# sudo mkfontdir
 sudo fc-cache
 sudo chmod 644 /usr/share/fonts/linux_fonts/*
